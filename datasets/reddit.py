1793b81810508cbf29648fea2651f5b621f35e86
Markus Städler <markus.staedler@hotmail.de>
2022-07-04 15:49:55 +0200

    Fix: Adapted reddit load code to new file compression used in online source

--- datasets/reddit.py
+++ datasets/reddit.py
@@ -3,19 +3,28 @@ This class provides the MovieLens 1M Dataset.
 See: https://grouplens.org/datasets/movielens/1m/
 """
 from absl import logging
-import bz2
+import io
 import json
 import pandas as pd
+import zstandard as zstd
 
 import datasets.dataset_utils as dataset_utils
 import bert4rec.utils as utils
 
 
 def load_reddit(debug: bool = False) -> pd.DataFrame:
-    url = 'https://files.pushshift.io/reddit/comments/RC_2011-01.bz2'
-    download_file = utils.get_virtual_env_path().joinpath("data", "reddit", "comments",  "RC_2011-01.bz2")
+    """
+    Load reddit data from a bz2 compressed data file in json format
+
+    :param debug:
+    :return:
+    """
+    category = "comments"
+    file_name = "RC_2011-01.zst"
+    url = f"https://files.pushshift.io/reddit/{category}/{file_name}"
+    download_file = utils.get_virtual_env_path().joinpath("data", "reddit", category,  file_name)
     # size in bytes of the fully downloaded dataset
-    download_size = 613090022
+    download_size = 621585706
 
     if not dataset_utils.is_available(download_file, download_size):
         logging.info("Raw data doesn't exist. Download...")
@@ -23,13 +32,17 @@ def load_reddit(debug: bool = False) -> pd.DataFrame:
     logging.info("Raw data already exists. Skip downloading")
 
     processed_input = []
-    with bz2.open(download_file, encoding="utf-8", mode="rt") as json_file:
-        if debug:
-            lines = json_file.readlines(1000000)
-        else:
-            lines = json_file.readlines()
-        for line in lines:
+    with open(download_file, mode="rb") as json_file:
+        # max window size to avoid memory overflow according to: https://stackoverflow.com/a/72092961
+        dctx = zstd.ZstdDecompressor(max_window_size=2147483648)
+        stream_reader = dctx.stream_reader(json_file)
+        text_stream = io.TextIOWrapper(stream_reader, encoding="utf-8")
+        line_counter = 0
+        for line in text_stream:
+            if debug and line_counter > 100000:
+                break
             processed_input.append(json.loads(line))
+            line_counter += 1
         json_file.close()
     df = pd.json_normalize(processed_input)
     return df
