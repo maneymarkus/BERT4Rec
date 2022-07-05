eb32ad021a42908fb19282d32c18bde5c05eaf38
Markus Städler <markus.staedler@hotmail.de>
2022-07-05 12:14:45 +0200

    Feature: Improved vocab exporting to be more robust

--- bert4rec/tokenizers/tokenizer_utils.py
+++ bert4rec/tokenizers/tokenizer_utils.py
@@ -1,7 +1,7 @@
 import pathlib
 
 
-def export_num_vocab_to_file(file_path: pathlib.Path, vocab: list):
+def export_num_vocab_to_file(file_path: pathlib.Path, vocab: list) -> bool:
     """
     Exports a given `vocab` (is of type list) to a specified `file_path`.
     Each line represents a single entry of the vocab and the "line number" depicts
@@ -9,11 +9,16 @@ def export_num_vocab_to_file(file_path: pathlib.Path, vocab: list):
 
     :param file_path: Location of vocab file to be put in
     :param vocab: Vocab list to be written to the filesystem
-    :return: None
+    :return: Boolean (True when everything was written to the file)
     """
+    if len(vocab) <= 0:
+        raise ValueError("The vocab that should be written to a file should have at least one entry!")
+
     with open(file_path, "w", encoding="utf-8") as file:
         for token in vocab:
             print(token, file=file)
+
+    return True
 
 
 def import_num_vocab_from_file(file_path: pathlib.Path) -> list[str]:
