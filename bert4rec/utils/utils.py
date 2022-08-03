fd60b82cb796e7596ff69711bbf187ccb1c1ffea
Markus Städler <markus.staedler@hotmail.de>
2022-08-03 10:51:48 +0200

    Refactor: Simplified paths in `utils.py` and added project root path

--- bert4rec/utils/utils.py
+++ bert4rec/utils/utils.py
@@ -6,18 +6,23 @@ import pathlib
 # Constants and Paths                             #
 ###################################################
 
-VIRTUAL_ENV_PATH = os.environ["VIRTUAL_ENV"]
-DEFAULT_MODEL_SAVE_PATH = pathlib.Path(os.environ["VIRTUAL_ENV"]).joinpath("saved_models")
-DEFAULT_MODEL_TRAIN_PATH = pathlib.Path("checkpoints/")
+VIRTUAL_ENV_PATH = pathlib.Path(os.environ["VIRTUAL_ENV"])
+DEFAULT_MODEL_SAVE_PATH = pathlib.Path("saved_models")
 
 
 def get_virtual_env_path() -> pathlib.Path:
-    return pathlib.Path(VIRTUAL_ENV_PATH)
+    return VIRTUAL_ENV_PATH
+
+
+def get_project_root() -> pathlib.Path:
+    return pathlib.Path(__file__).parent.parent.parent
 
 
 def get_default_model_save_path() -> pathlib.Path:
     return DEFAULT_MODEL_SAVE_PATH
 
 
-def get_default_model_train_path() -> pathlib.Path:
-    return DEFAULT_MODEL_TRAIN_PATH
+if __name__ == "__main__":
+    print(get_virtual_env_path())
+    print(get_project_root())
+    print(get_default_model_save_path())
