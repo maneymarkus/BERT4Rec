65f0e34eca4ba5738249c15ebf223debdf0ead05
Markus Städler <markus.staedler@hotmail.de>
2022-09-07 21:04:12 +0200

    Build: Upgraded to Tensorflow 2.10

--- bert4rec/model/components/networks/bert_encoder.py
+++ bert4rec/model/components/networks/bert_encoder.py
@@ -135,7 +135,7 @@ class BertEncoderV2(tf.keras.layers.Layer):
         # 'hidden_size'.
         self._embedding_projection = None
         if embedding_width != hidden_size:
-            self._embedding_projection = tf.keras.layers.experimental.EinsumDense(
+            self._embedding_projection = tf.keras.layers.EinsumDense(
                 '...x,xy->...y',
                 output_shape=hidden_size,
                 bias_axes='y',
@@ -427,7 +427,7 @@ class BertEncoder(tf.keras.Model):
         # We project the 'embedding' output to 'hidden_size' if it is not already
         # 'hidden_size'.
         if embedding_width != hidden_size:
-            embedding_projection = tf.keras.layers.experimental.EinsumDense(
+            embedding_projection = tf.keras.layers.EinsumDense(
                 '...x,xy->...y',
                 output_shape=hidden_size,
                 bias_axes='y',
