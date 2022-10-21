7b79d5744af558a344dc02e2bff24441e5483535
Markus Städler <markus.staedler@hotmail.de>
2022-10-21 11:28:20 +0200

    Feat: Added `sample_random_items_from_list()` function to evaluation utils

--- bert4rec/evaluation/evaluation_utils.py
+++ bert4rec/evaluation/evaluation_utils.py
@@ -1,9 +1,36 @@
 import copy
+import random
 
 
 def remove_elements_from_list(source: list, remove: list):
+    """
+    Removes elements in `remove` from a given list `source`
+
+    :param source:
+    :param remove:
+    :return:
+    """
     s = copy.copy(source)
     for r in remove:
         if r in s:
             s.remove(r)
     return s
+
+
+def sample_random_items_from_list(item_list: list, sample_size: int):
+    """
+    Samples `sample_size` items randomly from a given list `item_list`. If `sample_size` is greater than the
+    length of `item_list` than the `item_list` is simply shuffled and returned
+
+    :param item_list:
+    :param sample_size:
+    :return:
+    """
+    items = copy.copy(item_list)
+    random.shuffle(items)
+
+    if len(items) <= sample_size:
+        return items
+
+    random_items = [items.pop(0) for _ in range(sample_size)]
+    return random_items
