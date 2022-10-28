403c31baf89a989fd0074e18fe01f2f22c30efe2
maneymarkus <markus.staedler@hotmail.de>
2022-10-28 18:01:43 +0200

    Style: Simplified trainers factory method

--- bert4rec/trainers/__init__.py
+++ bert4rec/trainers/__init__.py
@@ -1,12 +1,8 @@
 from bert4rec.trainers.bert4rec_trainer import BERT4RecTrainer
 
 
-class TrainerFactory:
-    def get_trainer(self, model: str = "bert4rec", **kwargs):
-        if model == "bert4rec":
-            return BERT4RecTrainer(**kwargs)
-        else:
-            raise ValueError(f"{model} is not known!")
-
-
-trainer_factory = TrainerFactory()
+def get(identifier: str = "bert4rec", **kwargs):
+    if identifier == "bert4rec":
+        return BERT4RecTrainer(**kwargs)
+    else:
+        raise ValueError(f"{identifier} is not known!")
