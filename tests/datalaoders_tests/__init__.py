aaa9716d0b4218be768cc5c6138266ac5cdd8902
Markus Städler <markus.staedler@hotmail.de>
2022-12-01 12:35:38 +0100

    Fix: Fixed import errors in dataloaders tests

--- tests/datalaoders_tests/__init__.py
+++ tests/datalaoders_tests/__init__.py
@@ -1,3 +1,3 @@
 # exemplary only run tests for one of the dataloaders as these tests are really time-consuming
-from .bert4rec_dataloaders_tests import BERT4ML1MRecDataloaderTests
+from .bert4rec_dataloaders_tests import BERT4RecML1MDataloaderTests
 from .dataloader_utils_tests import DataloaderUtilsTest
