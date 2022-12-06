221ed15e9511965859e05a5ec8c40bb5283f897a
Markus Städler <markus.staedler@hotmail.de>
2022-12-06 19:00:05 +0100

    Feat: Added prediction mask to recommender to prevent it from predicting already known items

--- bert4rec/apps/recommender.py
+++ bert4rec/apps/recommender.py
@@ -14,6 +14,21 @@ class Recommender(tf.Module):
     def __call__(self, sequence: list[str]):
         model_input = self.dataloader.prepare_inference(sequence)
 
+        # create prediction mask from sequence ids to prevent these tokens from being predicted
+        # (because the user has already seen them and the recommender should predict something new)
+        sequence_ids = self.dataloader.get_tokenizer().tokenize(sequence)
+        # sort list because to_dense operation needs a sorted tensor
+        sequence_ids.sort()
+        # create a tensor of shape (#num_special_tokens, 1) from sequence ids list
+        skip_ids = tf.constant(sequence_ids, dtype=tf.int64)[:, None]
+        sparse_mask = tf.SparseTensor(
+            values=[-float("inf")] * len(skip_ids),
+            indices=skip_ids,
+            # match the shape (or simply the length) of the vocabulary
+            dense_shape=[self.dataloader.get_tokenizer().get_vocab_size()]
+        )
+        prediction_mask = tf.sparse.to_dense(sparse_mask)
+
         predictions = self.recommender_model(model_input, training=False)
 
         if "mlm_logits" in predictions:
@@ -37,6 +52,9 @@ class Recommender(tf.Module):
             if hasattr(self.recommender_model, "prediction_mask") \
                     and self.recommender_model.prediction_mask is not None:
                 vocab_logits += self.recommender_model.prediction_mask
+
+        # apply prediction mask generated above from the sequence ids
+        vocab_logits += prediction_mask
 
         # get most probable vocab index (or simply token)
         vocab_index = tf.argmax(vocab_logits[0])
