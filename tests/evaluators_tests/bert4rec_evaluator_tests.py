41ee77e563ba6b7f9a276b72b44202572a8d8ac3
Markus Städler <markus.staedler@hotmail.de>
2022-12-18 19:11:18 +0100

    Refactor: Adapted evaluation test code to newest changes in evaluation and sampler code

--- tests/evaluators_tests/bert4rec_evaluator_tests.py
+++ tests/evaluators_tests/bert4rec_evaluator_tests.py
@@ -3,7 +3,7 @@ import copy
 import random
 import tensorflow as tf
 
-from bert4rec.dataloaders import dataloader_utils
+from bert4rec.dataloaders import dataloader_utils, samplers
 import bert4rec.evaluation as evaluation
 from bert4rec.evaluation import evaluation_metrics
 from bert4rec.models import BERT4RecModel, BERT4RecModelWrapper
@@ -14,16 +14,16 @@ import tests.test_utils as test_utils
 
 class Bert4RecEvaluatorTest(tf.test.TestCase):
     def setUp(self):
-        super(Bert4RecEvaluatorTest, self).setUp()
+        super().setUp()
         logging.set_verbosity(logging.DEBUG)
-        self.evaluator = evaluation.get()
 
     def tearDown(self):
-        self.evaluator = None
+        super().tearDown()
+        pass
 
     def _create_model_wrapper(self, config_identifier: str = "ml-1m_128.json", vocab_size: int = 1000):
         # load a specific config
-        config_path = utils.get_project_root().joinpath(f"config/bert_train_configs/{config_identifier}")
+        config_path = utils.get_project_root().joinpath(f"config/bert4rec_train_configs/{config_identifier}")
         config = utils.load_json_config(config_path)
 
         bert_encoder = networks.Bert4RecEncoder(vocab_size, **config)
@@ -42,18 +42,18 @@ class Bert4RecEvaluatorTest(tf.test.TestCase):
         prepared_batches = dataloader_utils.make_batches(prepared_ds, batch_size=5)
         vocab_size = dataloader.tokenizer.get_vocab_size()
 
+        vocab = dataloader.get_tokenizer().get_vocab()
+        tokenized_vocab = dataloader.get_tokenizer().tokenize(vocab)
+        sampler = samplers.RandomSampler(vocab=tokenized_vocab, sample_size=100)
+
+        evaluator = evaluation.get(sampler=sampler)
+
         config_id = "ml-1m_128.json"
         model_wrapper = self._create_model_wrapper(config_id, vocab_size)
 
-        # usage of set in between to eliminate duplicates
-        random_popular_items = list(set([random.randint(0, vocab_size)
-                                         for _ in range(200)]))
+        metric_objects = evaluator.evaluate(model_wrapper, prepared_batches)
 
-        metric_objects = self.evaluator.evaluate(
-            model_wrapper, prepared_batches, tokenized_ds_item_list=random_popular_items
-        )
-
-        metrics = self.evaluator.get_metrics_results()
+        metrics = evaluator.get_metrics_results()
 
         self.assertEqual(metrics["Valid Ranks"], ds_size,
                          f"The metrics should have as many valid ranks (basically how many ranks have been "
@@ -77,39 +77,41 @@ class Bert4RecEvaluatorTest(tf.test.TestCase):
             evaluation_metrics.NDCG(100)
         ]
 
-        self.evaluator = evaluation.get(**{"metrics": metrics})
-
         prepared_ds, dataloader = test_utils.generate_random_sequence_dataset(ds_size=ds_size, seq_max_len=max_seq_len)
         prepared_batches = dataloader_utils.make_batches(prepared_ds, batch_size=5)
         vocab_size = dataloader.tokenizer.get_vocab_size()
+
+        vocab = dataloader.get_tokenizer().get_vocab()
+        tokenized_vocab = dataloader.get_tokenizer().tokenize(vocab)
+        sampler = samplers.RandomSampler(vocab=tokenized_vocab, sample_size=100)
+
+        evaluator = evaluation.get(metrics=metrics, sampler=sampler)
 
         config_id = "ml-1m_128.json"
         model_wrapper = self._create_model_wrapper(config_id, vocab_size)
 
         initial_metrics = [
-            copy.copy(metric_object) for metric_object in self.evaluator.get_metrics()
+            copy.copy(metric_object) for metric_object in evaluator.get_metrics()
         ]
 
         # usage of set in between to eliminate duplicates
         random_popular_items = list(set([random.randint(0, vocab_size)
                                          for _ in range(200)]))
 
-        metrics = self.evaluator.evaluate(
-            model_wrapper, prepared_batches, tokenized_ds_item_list=random_popular_items
-        )
+        metrics = evaluator.evaluate(model_wrapper, prepared_batches)
 
         for i, metric in enumerate(metrics):
             self.assertNotEqual(metric.result(), initial_metrics[i].result(),
                                 f"After evaluating the metrics should be filled and not in their initial state "
                                 f"anymore.")
 
-        self.evaluator.reset_metrics()
+        evaluator.reset_metrics()
 
-        for i, metric in enumerate(self.evaluator.get_metrics()):
+        for i, metric in enumerate(evaluator.get_metrics()):
             self.assertEqual(metric.result(), initial_metrics[i].result(),
                              f"After resetting the metrics they should be in their initial state again.\n"
                              f"Expected: {initial_metrics}\n"
-                             f"Got: {self.evaluator.get_metrics()}")
+                             f"Got: {evaluator.get_metrics()}")
 
 
 if __name__ == '__main__':
