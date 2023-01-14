d40a6bf40eab52053a3a320284d8ab5c4de39a68
Markus Städler <markus.staedler@hotmail.de>
2023-01-14 19:26:37 +0100

    Refactor: Refactored beauty example

--- examples/bert4rec_beauty_example.py
+++ examples/bert4rec_beauty_example.py
@@ -14,18 +14,18 @@ import bert4rec.utils as utils
 def main():
     # os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
 
-    EPOCHS = 150
+    EPOCHS = 200
 
     # set logging to most verbose level
     logging.set_verbosity(logging.DEBUG)
 
     dataloader_factory = get_dataloader_factory("bert4rec")
     dataloader_config = {
-        "input_duplication_factor": 10
+        "input_duplication_factor": 5
     }
     dataloader = dataloader_factory.create_beauty_dataloader(**dataloader_config)
     dataloader.generate_vocab()
-    train_ds, val_ds, test_ds = dataloader.prepare_training(finetuning_split=0.15)
+    train_ds, val_ds, test_ds = dataloader.prepare_training()
     tokenizer = dataloader.get_tokenizer()
 
     # load a specific config
@@ -42,30 +42,22 @@ def main():
     trainer = trainers.get(**{"model": model})
     trainer.initialize_model()
 
-    save_path = model_utils.determine_model_path(pathlib.Path("bert4rec_beauty"))
+    save_path = model_utils.determine_model_path(pathlib.Path("bert4rec_beauty_2"))
     # is needed as this does not create a new folder but rather the base name (or prefix)
     # for the created checkpoint files
     checkpoint_path = save_path.joinpath("checkpoints")
 
-    train_batches = dataloader_utils.make_batches(train_ds, batch_size=256, reshuffle_each_iteration=True)
-    val_batches = dataloader_utils.make_batches(val_ds, batch_size=256, reshuffle_each_iteration=True)
+    train_batches = dataloader_utils.make_batches(train_ds, batch_size=256)
+    val_batches = dataloader_utils.make_batches(val_ds, batch_size=256)
     test_batches = dataloader_utils.make_batches(test_ds, batch_size=256)
 
-    # TODO: save and load dataset to save time using the official tf.data.Dataset.save() and .load() api
-    # not urgent anymore as tensors tend to have a much faster computation time (especially concerning
-    # shuffle buffer filling) than ragged tensors
-    # See: https://stackoverflow.com/a/67781967
-
-    # set up a training loop callback
-    early_stopping_callback = tf.keras.callbacks.EarlyStopping(
-        monitor="val_loss",
-        patience=15,
-        verbose=1
-    )
-    trainer.append_callback(early_stopping_callback)
-
     # train the model
-    trainer.train(train_batches, val_batches, checkpoint_path=checkpoint_path, epochs=EPOCHS)
+    trainer.train(train_batches,
+                  val_batches,
+                  checkpoint_path=checkpoint_path,
+                  epochs=EPOCHS,
+                  steps_per_epoch=500,
+                  validation_steps=10000)
     trainer.update_wrapper_meta_info(model_wrapper, dataloader)
 
     evaluator = BERT4RecEvaluator(dataloader=dataloader)
